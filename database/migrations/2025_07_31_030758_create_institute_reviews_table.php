88ccbcefbed64bbe70d25dfb1cdcf4c849e52f22
IdjiotSandwiches <idjiotsandwiches@gmail.com>
2025-07-31 11:40:48 +0700

    feat: finalize table migrations

---
+++ database/migrations/2025_07_31_030758_create_institute_reviews_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('institute_reviews', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('rating');
+            $table->text('description')->nullable();
+            $table->unsignedBigInteger('reviewer_id');
+            $table->unsignedBigInteger('institute_id');
+            $table->timestamps();
+
+            $table->foreign('reviewer_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('institute_id')->references('user_id')->on('institute')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('institute_reviews');
+    }
+};
---
+++ database/migrations/2025_07_31_042636_create_enrolled_courses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('enrolled_courses', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('is_complete');
+            $table->boolean('is_verified');
+            $table->unsignedBigInteger('course_schedule_id');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('course_schedule_id')->references('id')->on('course_schedules')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('enrolled_courses');
+    }
+};
