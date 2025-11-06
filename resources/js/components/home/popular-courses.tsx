1f5ca544b0c3213a12ab0d9e0e4011bd0b8debe7
StephenEpen <stephentio.23@gmail.com>
2025-11-06 21:45:26 +0700

    style: modified appearance

--- resources/js/components/home/popular-courses.tsx
+++ resources/js/components/home/popular-courses.tsx
@@ -5,7 +5,7 @@ export default function PopularCourses({ courses }: { courses: any[] }) {
   return (
     <section className="py-16 bg-[#F7FDFD] px-6 md:px-12">
       <div className="flex items-center justify-between mb-8">
-        <h2 className="text-xl md:text-2xl font-semibold text-gray-800">
+        <h2 className="text-lg md:text-xl font-semibold text-gray-800">
           Popular Courses
         </h2>
         <button className="text-[#3ABEFF] font-medium hover:underline text-sm" onClick={() => router.get(route('list-course'))}>
