6d9807e1b8f5e04c4dab6a81ade98b9ec561cc0b
IdjiotSandwiches <idjiotsandwiches@gmail.com>
2025-12-16 19:41:47 +0700

    fix: page title

--- resources/js/layouts/lms-layout.tsx
+++ resources/js/layouts/lms-layout.tsx
@@ -1,6 +1,6 @@
 import MobileNavbar from '@/components/lms/dashboard-navbar';
 import Sidebar from '@/components/lms/sidebar';
-import { router, usePage } from '@inertiajs/react';
+import { Head, router, usePage } from '@inertiajs/react';
 import { useEffect, useState } from 'react';
 import { toast } from 'sonner';
 
@@ -23,22 +23,25 @@ export default function LMSLayout({ children, title = 'Dashboard' }: any) {
     }, []);
 
     return (
-        <div className="flex min-h-[100svh] w-full bg-gray-50 md:h-screen">
-            <Sidebar
-                collapsed={collapsed}
-                mobileOpen={mobileOpen}
-                onClose={() => setMobileOpen(false)}
-                onToggleCollapse={() => setCollapsed(!collapsed)}
-                onNavigate={() => setMobileOpen(false)}
-            />
+        <>
+            <Head title={title} />
+            <div className="flex min-h-[100svh] w-full bg-gray-50 md:h-screen">
+                <Sidebar
+                    collapsed={collapsed}
+                    mobileOpen={mobileOpen}
+                    onClose={() => setMobileOpen(false)}
+                    onToggleCollapse={() => setCollapsed(!collapsed)}
+                    onNavigate={() => setMobileOpen(false)}
+                />
 
-            <main className="relative flex flex-1 flex-col overflow-y-auto 2xl:px-36">
-                <div className="mb-20 md:mb-0">
-                    <MobileNavbar title={title} onMenu={() => setMobileOpen(true)} />
-                </div>
+                <main className="relative flex flex-1 flex-col overflow-y-auto 2xl:px-36">
+                    <div className="mb-20 md:mb-0">
+                        <MobileNavbar title={title} onMenu={() => setMobileOpen(true)} />
+                    </div>
 
-                <div className="flex flex-1 flex-col p-6 md:p-8">{children}</div>
-            </main>
-        </div>
+                    <div className="flex flex-1 flex-col p-6 md:p-8">{children}</div>
+                </main>
+            </div>
+        </>
     );
 }
