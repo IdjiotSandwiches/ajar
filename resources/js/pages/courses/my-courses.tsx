17f410dde41fb76b61f76707c99ae79cb05d1333
StephenEpen <stephentio.23@gmail.com>
2025-12-16 11:06:09 +0700

    chore: make filter to separate component

--- resources/js/pages/courses/my-courses.tsx
+++ resources/js/pages/courses/my-courses.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-wrapper-object-types */
+import Filter from '@/components/lms/filter';
 import DynamicModal from '@/components/modal/modal';
 import Pagination from '@/components/pagination';
 import LMSLayout from '@/layouts/lms-layout';
@@ -9,15 +10,6 @@ import React, { useState } from 'react';
 export default function CourseList({ courses }: { courses: any }) {
     const [showModal, setShowModal] = useState(false);
     const [deleteCourse, setDeleteCourse] = useState<Number>();
-    const [filters, setFilters] = useState({
-        search: '',
-        category: '',
-        minPrice: '',
-        maxPrice: '',
-        minDuration: '',
-        maxDuration: '',
-    });
-
 
     const handleDeleteClick = (id: number) => {
         setDeleteCourse(id);
@@ -33,89 +25,13 @@ export default function CourseList({ courses }: { courses: any }) {
         router.get(route('institute.course-detail', courseId));
     };
 
-    const handleFilterChange = (key: string, value: string) => {
-        setFilters(prev => ({
-            ...prev,
-            [key]: value,
-        }));
-    };
-
-    const applyFilter = () => {
-        router.get(
-            route('institute.courses'),
-            {
-                ...filters,
-            },
-            {
-                preserveState: true,
-                replace: true,
-            }
-        );
-    };
-
     return (
         <>
             <Head title="My Courses" />
             <div className="flex min-h-screen flex-col gap-6">
                 <h1 className="hidden md:flex text-2xl font-semibold text-gray-800">My Courses</h1>
 
-                <div className="grid grid-cols-1 gap-4 md:grid-cols-4 rounded-2xl bg-white/80 p-4 shadow-sm backdrop-blur-sm sm:p-6 md:p-8">
-                    <div className="w-full">
-                        <p className="text-sm mb-1 font-medium">Search by title</p>
-                        <input
-                            type="text"
-                            placeholder="Search course"
-                            value={filters.search}
-                            onChange={e => handleFilterChange('search', e.target.value)}
-                            className="rounded-md border border-gray-300 px-3 py-2 text-sm focus:border-[#42C2FF] focus:ring-[#42C2FF] w-full"
-                        />
-                    </div>
-
-                    <div className="w-full">
-                        <p className="text-sm mb-1 font-medium">Category</p>
-                        <select
-                            value={filters.category}
-                            onChange={e => handleFilterChange('category', e.target.value)}
-                            className="rounded-md border border-gray-300 px-3 py-2 text-sm focus:border-[#42C2FF] w-full"
-                        >
-                            <option value="">All Category</option>
-                            <option value="programming">Programming</option>
-                            <option value="design">Design</option>
-                            <option value="business">Business</option>
-                        </select>
-                    </div>
-
-                    <div className="w-full">
-                        <p className="text-sm mb-1 font-medium">Minimum Price</p>
-                        <input
-                            type="number"
-                            min={0}
-                            placeholder="Rp0,00"
-                            value={filters.minPrice}
-                            onChange={e => handleFilterChange('minPrice', e.target.value)}
-                            className="rounded-md border border-gray-300 px-3 py-2 text-sm w-full"
-                        />
-
-                    </div>
-                    <div className="w-full">
-                        <p className="text-sm mb-1 font-medium">Maximum Price</p>
-                        <input
-                            type="number"
-                            min={0}
-                            placeholder="Rp100.000,00"
-                            value={filters.maxPrice}
-                            onChange={e => handleFilterChange('maxPrice', e.target.value)}
-                            className="rounded-md border border-gray-300 px-3 py-2 text-sm w-full"
-                        />
-                    </div>
-
-                    {/* <button
-                        onClick={applyFilter}
-                        className="rounded-md bg-[#42C2FF] px-4 py-2 text-sm font-medium text-white hover:bg-[#42C2FF]/90"
-                    >
-                        Apply
-                    </button> */}
-                </div>
+                <Filter />
 
                 <div className="mx-auto w-full rounded-2xl bg-white/80 p-4 shadow-sm backdrop-blur-sm sm:p-6 md:p-8">
                     <div className="mb-6 flex justify-between items-center">
