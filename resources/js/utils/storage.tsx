881a8c23cd0273d28b083604cffe7c60107c7197
IdjiotSandwiches <idjiotsandwiches@gmail.com>
2025-12-26 12:47:48 +0700

    fix: seeder & blob

--- resources/js/utils/storage.tsx
+++ resources/js/utils/storage.tsx
@@ -1,4 +1,4 @@
-export function storageUrl(path: any, placeholder = 'https://placehold.co/40') {
+export function storageUrl(path: any, placeholder = 'https://placehold.co/400') {
     if (!path) return placeholder;
     if (/^(https?:|blob:)/.test(path)) return path;
     return `/storage/${path}`;
