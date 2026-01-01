150e61fcf53efb72a40962b31501b97ba5697098
IdjiotSandwiches <idjiotsandwiches@gmail.com>
2026-01-01 09:20:05 +0700

    fix: application status retrieval

--- resources/js/pages/chat/layout.tsx
+++ resources/js/pages/chat/layout.tsx
@@ -2,13 +2,14 @@ import useMediaQuery from '@/hooks/use-media-query';
 import LMSLayout from '@/layouts/lms-layout';
 import { router, usePage } from '@inertiajs/react';
 import { debounce } from 'lodash';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import UserList from '../../components/chat/user-list';
 
 export default function ChatLayout({ children }: any) {
     const { props } = usePage();
     const user = props.auth?.user;
     const isMobile = useMediaQuery('(max-width: 1024px)');
+    const [isChatOpen, setIsChatOpen] = useState(false);
 
     useEffect(() => {
         if (!user) return;
@@ -37,6 +38,21 @@ export default function ChatLayout({ children }: any) {
     }, []);
 
     if (isMobile) {
+        return (
+            <LMSLayout title="Messages">
+                <div className="flex flex-1 flex-col overflow-hidden rounded-2xl border shadow-sm dark:border-white/20 dark:shadow-[#ffffff]/20">
+                    <div className="rounded-2xl border shadow-sm dark:border-white/20 dark:shadow-[#ffffff]/20">
+                        <div className="flex items-center gap-2 border-b px-4 py-3 dark:border-white/20">
+                            <h2 className="text-lg font-semibold text-gray-700 dark:text-white">Chats</h2>
+                        </div>
+                        <UserList />
+                    </div>
+                    <div className="ml-4 flex flex-1 flex-col rounded-2xl border p-6 shadow-sm dark:border-white/20 dark:shadow-[#ffffff]/20">
+                        {children}
+                    </div>
+                </div>
+            </LMSLayout>
+        );
     }
 
     return (
