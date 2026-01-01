5d17f9c2ec3990cc804a8be067cd72d43a62b82b
IdjiotSandwiches <idjiotsandwiches@gmail.com>
2026-01-01 09:51:41 +0700

    fix: not give admin to access chat

--- resources/js/components/teacher/card.tsx
+++ resources/js/components/teacher/card.tsx
@@ -1,9 +1,13 @@
 import { storageUrl } from '@/utils/storage';
-import { router } from '@inertiajs/react';
+import { router, usePage } from '@inertiajs/react';
 import { Send } from 'lucide-react';
 import { FaGithub, FaInstagram, FaLinkedinIn } from 'react-icons/fa';
 
 export default function TeacherProfileCard({ teacher }: any) {
+    const { props } = usePage();
+    const user = props.auth?.user;
+    const roles = props.enums?.roles_enum;
+
     const icons: any = {
         Github: FaGithub,
         Instagram: FaInstagram,
@@ -11,21 +15,22 @@ export default function TeacherProfileCard({ teacher }: any) {
     };
 
     return (
-        <div className="rounded-2xl bg-[#3ABEFF] dark:bg-[#222831] p-1 shadow-lg">
+        <div className="rounded-2xl bg-[#3ABEFF] p-1 shadow-lg dark:bg-[#222831]">
             <div className="rounded-2xl bg-white p-0.5">
-                <div className="relative flex flex-col items-center overflow-hidden rounded-2xl bg-[#3ABEFF] dark:bg-[#222831] p-6 text-center">
+                <div className="relative flex flex-col items-center overflow-hidden rounded-2xl bg-[#3ABEFF] p-6 text-center dark:bg-[#222831]">
                     <div className="z-10 mb-4 h-28 w-28 overflow-hidden rounded-full border-2 border-white shadow-md">
-                        <img
-                            src={storageUrl(teacher.user?.profile_picture)}
-                            alt={teacher.user?.name}
-                            className="h-full w-full object-cover"
-                        />
+                        <img src={storageUrl(teacher.user?.profile_picture)} alt={teacher.user?.name} className="h-full w-full object-cover" />
                     </div>
                     <h2 className="z-10 cursor-default text-xl font-bold text-white">{teacher.user?.name}</h2>
                     <p className="z-10 mb-4 cursor-default text-sm text-white/90">{teacher.description}</p>
-                    <button onClick={() => router.visit(route('chat.show', teacher.user?.uuid))} className="z-10 mb-5 flex cursor-pointer items-center gap-2 rounded-lg bg-white dark:bg-white/90 px-4 py-2 font-medium text-[#3ABEFF] shadow transition hover:bg-blue-50">
-                        <Send size={18} /> Send Message
-                    </button>
+                    {user?.role_id !== roles.Admin && (
+                        <button
+                            onClick={() => router.visit(route('chat.show', teacher.user?.uuid))}
+                            className="z-10 mb-5 flex cursor-pointer items-center gap-2 rounded-lg bg-white px-4 py-2 font-medium text-[#3ABEFF] shadow transition hover:bg-blue-50 dark:bg-white/90"
+                        >
+                            <Send size={18} /> Send Message
+                        </button>
+                    )}
                     <div className="z-10 flex cursor-pointer gap-8 text-xl text-white">
                         {(() => {
                             const socials = teacher?.user?.social_medias ?? [];
