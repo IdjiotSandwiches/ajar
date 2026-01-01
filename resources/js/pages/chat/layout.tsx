0b36c4feea8ea0c7843e15a7fc3b429b6c1f3f9e
IdjiotSandwiches <idjiotsandwiches@gmail.com>
2026-01-01 09:41:40 +0700

    fix: chat layout

--- resources/js/pages/chat/layout.tsx
+++ resources/js/pages/chat/layout.tsx
@@ -6,13 +6,12 @@ import { useEffect, useState } from 'react';
 import UserList from '../../components/chat/user-list';
 
 export default function ChatLayout({ children }: any) {
-    const { props } = usePage();
-    const user = props.auth?.user;
+    const { chat_with: chatWithUser, auth }: any = usePage().props;
     const isMobile = useMediaQuery('(max-width: 1024px)');
     const [isChatOpen, setIsChatOpen] = useState(false);
 
     useEffect(() => {
-        if (!user) return;
+        if (!auth.user) return;
 
         const debouncedReload = debounce(() => {
             router.reload({
@@ -20,7 +19,7 @@ export default function ChatLayout({ children }: any) {
             });
         }, 350);
 
-        window.Echo.private(`message.${user.uuid}`)
+        window.Echo.private(`message.${auth.user.uuid}`)
             .listen('ReadMessageEvent', () => {
                 debouncedReload();
             })
@@ -29,7 +28,7 @@ export default function ChatLayout({ children }: any) {
             });
 
         return () => {
-            window.Echo.private(`message.${user.uuid}`)
+            window.Echo.private(`message.${auth.user.uuid}`)
                 .stopListening('ReadMessageEvent', () => {
                     debouncedReload();
                 })
@@ -37,10 +36,23 @@ export default function ChatLayout({ children }: any) {
         };
     }, []);
 
-    if (isMobile) {
-        return (
-            <LMSLayout title="Messages">
+    return (
+        <LMSLayout title="Messages">
+            {isMobile ? (
                 <div className="flex flex-1 flex-col overflow-hidden rounded-2xl border shadow-sm dark:border-white/20 dark:shadow-[#ffffff]/20">
+                    {chatWithUser ? (
+                        <div className="flex w-full flex-col h-full">{children}</div>
+                    ) : (
+                        <div className="w-full">
+                            <div className="flex items-center gap-2 border-b px-4 py-3 dark:border-white/20">
+                                <h2 className="text-lg font-semibold text-gray-700 dark:text-white">Chats</h2>
+                            </div>
+                            <UserList />
+                        </div>
+                    )}
+                </div>
+            ) : (
+                <div className="flex max-h-[80vh]">
                     <div className="rounded-2xl border shadow-sm dark:border-white/20 dark:shadow-[#ffffff]/20">
                         <div className="flex items-center gap-2 border-b px-4 py-3 dark:border-white/20">
                             <h2 className="text-lg font-semibold text-gray-700 dark:text-white">Chats</h2>
@@ -51,23 +63,7 @@ export default function ChatLayout({ children }: any) {
                         {children}
                     </div>
                 </div>
-            </LMSLayout>
-        );
-    }
-
-    return (
-        <LMSLayout title="Messages">
-            <div className="flex max-h-[80vh]">
-                <div className="rounded-2xl border shadow-sm dark:border-white/20 dark:shadow-[#ffffff]/20">
-                    <div className="flex items-center gap-2 border-b px-4 py-3 dark:border-white/20">
-                        <h2 className="text-lg font-semibold text-gray-700 dark:text-white">Chats</h2>
-                    </div>
-                    <UserList />
-                </div>
-                <div className="ml-4 flex flex-1 flex-col rounded-2xl border p-6 shadow-sm dark:border-white/20 dark:shadow-[#ffffff]/20">
-                    {children}
-                </div>
-            </div>
+            )}
         </LMSLayout>
     );
 }
